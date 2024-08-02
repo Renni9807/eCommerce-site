be500ac6fd162bfa4cfb142c59245a3b4f599053
Renni9807 <practiceID98@gmail.com>
2024-08-02 20:35:56 +0800

    general page setup complete

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import whatsapp_icon from "../Assets/whatsapp_icon.png";
+import pintester_icon from "../Assets/pintester_icon.png";
+import instagram_icon from "../Assets/instagram_icon.png";
+import footer_logo from "../Assets/logo_big.png";
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-logo">
+        <img src={footer_logo} alt="logo" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="footer-links">
+        <li>Company</li>
+        <li>Products</li>
+        <li>Offices</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <div className="footer-social-icon">
+        <div className="footer-icons-container">
+          <img src={instagram_icon} alt="instagram" />
+          <img src={pintester_icon} alt="pintester" />
+          <img src={whatsapp_icon} alt="whatsapp" />
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <hr />
+        <p>Copyright @ 2024 - All Right Reserved.</p>
+      </div>
+    </div>
+  );
+};
